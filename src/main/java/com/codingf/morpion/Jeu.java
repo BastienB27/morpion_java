2f3dde65f76b836ed655a820429028c1b0e5b2c4
BastienVitour <bvitour2004@gmail.com>
2023-01-04 11:26:29 +0100

    début affichage grille

--- src/main/java/com/codingf/morpion/Jeu.java
+++ src/main/java/com/codingf/morpion/Jeu.java
@@ -2,5 +2,18 @@ package com.codingf.morpion;
 
 public class Jeu {
 
+    static void affichageGrille(){
+        System.out.println("  |  |  ");
+        System.out.println("──┼──┼──");
+        System.out.println("  |  |  ");
+        System.out.println("──┼──┼─");
+        System.out.println("  |  |  ");
+    }
+
+    public static void main(String[] args) {
+
+        affichageGrille();
+
+    }
 
 }
