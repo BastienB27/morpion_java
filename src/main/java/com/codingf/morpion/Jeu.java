391bb89e61c8e5b5582a7b4824a7e7f7389b02b8
Bouard Bastien <bastien.bouard27@gmail.com>
2023-01-04 15:13:16 +0100

    Conditions

--- src/main/java/com/codingf/morpion/Jeu.java
+++ src/main/java/com/codingf/morpion/Jeu.java
@@ -14,6 +14,57 @@ public class Jeu {
 
         affichageGrille();
 
+        // condition de victoire
+        int lenghboard = 9; //Changer en variable avec un inconnu
+        int nombreCoups = 0; //Compteur de nombre de coup
+        boolean victoire = false;
+
+
+        while(!victoire) {
+
+            if (case1 == case2 && case1 == case3 && case1 != " ") {
+                victoire = true;
+            }
+
+            if (case4 == case5 && case4 == case6 && case4 != " ") {
+                victoire = true;
+            }
+
+            if (case7 == case8 && case7 == case9 && case7 != " ") {
+                victoire = true;
+            }
+
+            if (case1 == case4 && case1 == case7 && case1 != " ") {
+                victoire = true;
+            }
+
+            if (case2 == case5 && case5 == case8 && case2 != " ") {
+                victoire = true;
+            }
+
+            if (case3 == case6 && case3 == case9 && case3 != " ") {
+                victoire = true;
+            }
+
+            if (case1 == case5 && case1 == case9 && case1 != " ") {
+                victoire = true;
+            }
+
+            if (case3 == case5 && case3 == case7 && case3 != " ") {
+                victoire = true;
+            }
+
+            nombreCoups++;
+            if (nombreCoups == lenghboard){
+                System.out.println("Egalité, pas de victoire");
+                break;
+            }
+        }
+
+        if(victoire){
+            System.out.println("Victoire de", symbole);
+        }
+
     }
 
 }
